dd38fd1ad1f54f8838efde5fc8c45c6465542580
mgudalewicz <michal.gudalewicz@gmail.com>
2022-02-23 00:46:12 +0100

    Install FirebaseAuth

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -14,7 +22,38 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: const RootPage(),
     );
+  }
+}
+
+class RootPage extends StatelessWidget {
+  const RootPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<Object>(
+        stream: null,
+        builder: (context, snapshot) {
+          return StreamBuilder<User?>(
+              stream: FirebaseAuth.instance.authStateChanges(),
+              builder: (context, snapshot) {
+                final user = snapshot.data;
+                if (user == null) {
+                  return const Scaffold(
+                    body: Center(
+                      child: Text('Jesteś niezalogowany'),
+                    ),
+                  );
+                }
+                return Scaffold(
+                  body: Center(
+                    child: Text('Jesteś zalogowany jako ${user.email}'),
+                  ),
+                );
+              });
+        });
   }
 }
