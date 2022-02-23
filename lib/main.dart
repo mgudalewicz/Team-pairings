db4802af59917ff2ea56b026127c624f27fd29d3
mgudalewicz <michal.gudalewicz@gmail.com>
2022-02-23 11:26:46 +0100

    Move myApp to app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:parowanie/app/features/home/home_page.dart';
-import 'package:parowanie/app/features/login/login_page.dart';
+import 'package:parowanie/app/app.dart';
 
 import 'firebase_options.dart';
 
@@ -14,40 +12,4 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const RootPage(),
-    );
-  }
-}
-
-class RootPage extends StatelessWidget {
-  const RootPage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<Object>(
-        stream: null,
-        builder: (context, snapshot) {
-          return StreamBuilder<User?>(
-              stream: FirebaseAuth.instance.authStateChanges(),
-              builder: (context, snapshot) {
-                final user = snapshot.data;
-                if (user == null) {
-                  return LoginPage();
-                }
-                return HomePage(user: user);
-              });
-        });
-  }
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:parowanie/app/app.dart';
 
 import 'package:parowanie/main.dart';
 
