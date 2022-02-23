6416789d4bd81e831e1c16004dc456183eda370b
mgudalewicz <michal.gudalewicz@gmail.com>
2022-02-23 11:21:36 +0100

    Move home and login to features folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:parowanie/app/home/home_page.dart';
-import 'package:parowanie/app/login/login_page.dart';
+import 'package:parowanie/app/features/home/home_page.dart';
+import 'package:parowanie/app/features/login/login_page.dart';
 
 import 'firebase_options.dart';
 
