a2c29883bcaab25c38052f58880836dc4db08251
mgudalewicz <michal.gudalewicz@gmail.com>
2022-02-23 01:42:12 +0100

    Add login functionality

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -1,15 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({
+  LoginPage({
     Key? key,
   }) : super(key: key);
 
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(title: const Text('Logowanie')),
       body: Center(
-        child: Text('Jesteś niezalogowany'),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'E-mail',
+                  hintText: 'Podaj swój e-mail',
+                ),
+                controller: emailController,
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Hasło',
+                  hintText: 'Podaj swoje hasło',
+                ),
+                controller: passwordController,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    await FirebaseAuth.instance.signInWithEmailAndPassword(
+                      email: emailController.text,
+                      password: passwordController.text,
+                    );
+                  } catch (error) {
+                    print(error);
+                  }
+                  ;
+                },
+                child: const Text('Zaloguj się'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
