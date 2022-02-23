8ac9582d249706e06126f8da9a998c51018c9633
mgudalewicz <michal.gudalewicz@gmail.com>
2022-02-23 02:51:47 +0100

    Add registration

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -14,12 +14,14 @@ class LoginPage extends StatefulWidget {
 }
 
 var errorMessage = '';
+var isCreatingAccount = false;
 
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Logowanie')),
+      appBar: AppBar(
+          title: Text(isCreatingAccount == true ? 'Rejestracja' : 'Logowanie')),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
@@ -45,32 +47,80 @@ class _LoginPageState extends State<LoginPage> {
                 controller: widget.passwordController,
               ),
               const SizedBox(height: 20),
-              Text(errorMessage),
+              Text(errorMessage, style: const TextStyle(color: Colors.red)),
               const SizedBox(height: 20),
               ElevatedButton(
-                onPressed: () async {
-                  try {
-                    await FirebaseAuth.instance.signInWithEmailAndPassword(
-                      email: widget.emailController.text,
-                      password: widget.passwordController.text,
-                    );
-                  } catch (error) {
-                    setState(() {
-                      if (error.toString() ==
-                          '[firebase_auth/unknown] Given String is empty or null') {
-                        errorMessage = 'Musisz uzupełnić pola';
-                      } else if (error.toString() ==
-                          '[firebase_auth/invalid-email] The email address is badly formatted.') {
-                        errorMessage = 'Tak nie wygląda adres e-mail';
-                      } else {
-                        errorMessage = error.toString();
+                  onPressed: () async {
+                    if (isCreatingAccount == true) {
+                      //rejestracja
+                      try {
+                        await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                          email: widget.emailController.text,
+                          password: widget.passwordController.text,
+                        );
+                      } catch (error) {
+                        setState(() {
+                          if (error.toString() ==
+                              '[firebase_auth/unknown] Given String is empty or null') {
+                            errorMessage = 'Musisz uzupełnić pola';
+                          } else if (error.toString() ==
+                              '[firebase_auth/invalid-email] The email address is badly formatted.') {
+                            errorMessage = 'Tak nie wygląda adres e-mail';
+                          } else if (error.toString() ==
+                              '[firebase_auth/weak-password] Password should be at least 6 characters') {
+                            errorMessage =
+                                'Hasło musi składać się przynajmniej z 6 znaków';
+                          } else {
+                            errorMessage = error.toString();
+                          }
+                          print(error);
+                        });
                       }
+                    } else if (isCreatingAccount == false) {
+                      try {
+                        await FirebaseAuth.instance.signInWithEmailAndPassword(
+                          email: widget.emailController.text,
+                          password: widget.passwordController.text,
+                        );
+                      } catch (error) {
+                        setState(() {
+                          if (error.toString() ==
+                              '[firebase_auth/unknown] Given String is empty or null') {
+                            errorMessage = 'Musisz uzupełnić pola';
+                          } else if (error.toString() ==
+                              '[firebase_auth/invalid-email] The email address is badly formatted.') {
+                            errorMessage = 'Tak nie wygląda adres e-mail';
+                          } else {
+                            errorMessage = error.toString();
+                          }
+                        });
+                      }
+                    }
+                  },
+                  child: Text(isCreatingAccount == true
+                      ? 'Zarejestruj się'
+                      : 'Zaloguj się')),
+              const SizedBox(height: 20),
+              if (isCreatingAccount == false) ...[
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      isCreatingAccount = true;
                     });
-                    print(error);
-                  }
-                },
-                child: const Text('Zaloguj się'),
-              ),
+                  },
+                  child: const Text('Utwórz konto'),
+                ),
+              ] else if (isCreatingAccount == true) ...[
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      isCreatingAccount = false;
+                    });
+                  },
+                  child: const Text('Masz juz konto?'),
+                ),
+              ],
             ],
           ),
         ),
