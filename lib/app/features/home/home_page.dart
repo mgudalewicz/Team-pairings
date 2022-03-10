fa41356eafbd4165b6b9dcded0f80ba983b3b860
mgudalewicz <michal.gudalewicz@gmail.com>
2022-03-10 11:31:59 +0100

    Add barto HomePage

--- lib/app/features/home/home_page.dart
+++ lib/app/features/home/home_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:parowanie/app/cubit/root_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({
+class HomePage extends StatefulWidget {
+  HomePage({
     Key? key,
     required this.user,
   }) : super(key: key);
@@ -11,22 +11,63 @@ class HomePage extends StatelessWidget {
   final user;
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Jesteś zalogowany jako ${user.email}'),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                context.read<RootCubit>().signOut();
-              },
-              child: const Text('Wyloguj'),
-            ),
-          ],
-        ),
+      body: Builder(builder: (context) {
+        if (currentIndex == 0) {
+          return Center(
+            child: Text('Jeden'),
+          );
+        }
+        if (currentIndex == 1) {
+          return Center(
+            child: Text('Dwa'),
+          );
+        }
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Jesteś zalogowany jako ${widget.user.email}'),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  context.read<RootCubit>().signOut();
+                },
+                child: const Text('Wyloguj'),
+              ),
+            ],
+          ),
+        );
+      }),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (newIndex) {
+          setState(() {
+            currentIndex = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: 'Zawodnicy',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.leaderboard),
+            label: 'Statystki',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Konto',
+          ),
+        ],
       ),
     );
   }
