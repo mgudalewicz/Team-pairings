5e33c924f29e374d1bfcaa1ec9d212fa5e357a47
mgudalewicz <michal.gudalewicz@gmail.com>
2022-03-21 21:08:22 +0100

    Add add page

--- lib/app/features/home/add/add_page.dart
+++ lib/app/features/home/add/add_page.dart
@@ -6,11 +6,47 @@ class AddPage extends StatelessWidget {
   }) : super(key: key);
 
   final controller = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Dodaj gracza'),
+      ),
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Zapomniałeś czegoś dodać :(';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Nick',
+                    hintText: 'Podaj imię zawodnika',
+                  ),
+                  controller: controller,
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      print("coś");
+                    }
+                  },
+                  child: const Text('Dodaj gracza'))
+            ],
+          ),
+        ),
       ),
     );
   }
