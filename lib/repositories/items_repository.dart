84f01d37869e24bd2569cd154dd240029f066b49
mgudalewicz <michal.gudalewicz@gmail.com>
2022-06-28 22:40:18 +0200

    Use user id for firebase

--- lib/repositories/items_repository.dart
+++ lib/repositories/items_repository.dart
@@ -1,9 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:parowanie/app/models/item_model.dart';
 
 class ItemsRepository {
   Stream<List<ItemsModel>> getItemsStream() {
-    return FirebaseFirestore.instance.collection('items').orderBy('name', descending: false).snapshots().map(
+    final userId = FirebaseAuth.instance.currentUser?.uid;
+    if (userId == null) {
+      throw Exception('User is not logged in');
+    }
+    return FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .collection('items')
+        .orderBy('name', descending: false)
+        .snapshots()
+        .map(
       (querySnapshot) {
         return querySnapshot.docs.map(
           (doc) {
@@ -26,15 +37,32 @@ class ItemsRepository {
   }
 
   Future<void> deleted({required String id}) {
-    return FirebaseFirestore.instance.collection('items').doc(id).delete();
+    final userId = FirebaseAuth.instance.currentUser?.uid;
+    if (userId == null) {
+      throw Exception('User is not logged in');
+    }
+    return FirebaseFirestore.instance.collection('users').doc(userId).collection('items').doc(id).delete();
   }
 
   Future<void> chamgeValue({required bool value, required String id}) {
-    return FirebaseFirestore.instance.collection('items').doc(id).update({'value': value});
+    final userId = FirebaseAuth.instance.currentUser?.uid;
+    if (userId == null) {
+      throw Exception('User is not logged in');
+    }
+    return FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .collection('items')
+        .doc(id)
+        .update({'value': value});
   }
 
   Future<void> addPlayers({required String text}) {
-    return FirebaseFirestore.instance.collection('items').add({
+    final userId = FirebaseAuth.instance.currentUser?.uid;
+    if (userId == null) {
+      throw Exception('User is not logged in');
+    }
+    return FirebaseFirestore.instance.collection('users').doc(userId).collection('items').add({
       'name': text,
       'goalsConceded': 0,
       'goalsScored': 0,
@@ -48,6 +76,10 @@ class ItemsRepository {
   }
 
   Future<void> endMatch({required String id, required int goalsConceded, required int goalsScored}) {
+    final userId = FirebaseAuth.instance.currentUser?.uid;
+    if (userId == null) {
+      throw Exception('User is not logged in');
+    }
     int win = 0;
     int lost = 0;
     int draw = 0;
@@ -62,7 +94,7 @@ class ItemsRepository {
       draw++;
       score += 1;
     }
-    return FirebaseFirestore.instance.collection('items').doc(id).update({
+    return FirebaseFirestore.instance.collection('users').doc(userId).collection('items').doc(id).update({
       'goalsConceded': FieldValue.increment(goalsConceded),
       'goalsScored': FieldValue.increment(goalsScored),
       'matches': FieldValue.increment(1),
